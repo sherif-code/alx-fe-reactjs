d50a166b4e8f13d40f3d7ab1f3890cbfa0083668
sherif rabiw karim <rabiwkarim55@gmail.com>
2025-07-23 13:13:12 +0000

    added styling

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <div>
+      <header
+        style={{
+          backgroundColor: "#00ad23",
+          color: "white",
+          textAlign: "center",
+        }}
+      >
+        <h1>My Favorite Cities</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
