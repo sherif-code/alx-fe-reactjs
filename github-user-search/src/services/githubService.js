c828d52ed20755fdd10bac110c85a2633f6f701d
sherif rabiw karim <rabiwkarim55@gmail.com>
2025-08-03 22:16:22 +0000

    update : error corrected

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+};
+
+export const advancedUserSearch = async ({ username, location, minRepos }) => {
+  let query = "";
+
+  if (username) query += `${username} in:login `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>=${minRepos}`;
+
+  const response = await axios.get(
+    `https://api.github.com/search/users?q=${encodeURIComponent(query)}`
+  );
+
+  return response.data;
+};
