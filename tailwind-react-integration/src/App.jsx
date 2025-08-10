2285bcfc6b69f5c756d19071c532196f51232c66
sherif rabiw karim <rabiwkarim55@gmail.com>
2025-08-10 19:44:17 +0000

    Tail wind styling implememted

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,17 +1,12 @@
-
-import './App.css'
-import UserProfile from './components/UserProfile '
+import React from "react";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-
-
   return (
     <>
-      
-       
-    <UserProfile />
+      <UserProfile />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
