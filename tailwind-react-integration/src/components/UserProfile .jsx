c97d73161e2730d0f66dc291f3a87e50dced9c36
sherif rabiw karim <rabiwkarim55@gmail.com>
2025-08-10 19:34:42 +0000

    nw styling

--- tailwind-react-integration/src/components/UserProfile .jsx
+++ tailwind-react-integration/src/components/UserProfile .jsx
@@ -1,12 +1,9 @@
-import profilePic from "../assets/sherif.jpg"
-
-
 function UserProfile() {
   return (
     <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img
-        src={profilePic}
-        alt="User-logo"
+        src="https://via.placeholder.com/150"
+        alt="User"
         className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="text-lg sm:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">
@@ -14,7 +11,7 @@ function UserProfile() {
       </h1>
       <p className="text-sm sm:text-base text-gray-600">
         Developer at Example Co. Loves to write code and explore new
-        technologies .
+        technologies.
       </p>
     </div>
   );
